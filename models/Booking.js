c7a46b82fcbd312a29ca9139d5ca37c1302f0a3a
BuildTools <unconfigured@null.spigotmc.org>
2024-03-22 03:43:06 +0700

    update

--- models/Booking.js
+++ models/Booking.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 const User = require("./User_info")
+const start = new Date();
+const end = new Date(start.getTime() + (1 * 60 * 60 * 1000));
+
 
 const BookSchema = new mongoose.Schema({
-    park_zone : {type : String, ref : 'User'}
+    book_id: mongoose.Schema.Types.ObjectId,
+    booking_start: { type: String, default: start.toLocaleTimeString() },
+    booking_end: { type: String, default: end.toLocaleTimeString() },
+    book_status : Boolean
     })
 
 module.exports = mongoose.model('Book' , BookSchema)
